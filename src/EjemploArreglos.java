ea3f27d6d8573b1ec427b51dece7a68e543634f6
Alejandro Arteaga <alejow18@gmail.com>
2021-08-15 20:23:41 -0500

    Manejo de arreglos con java

---
+++ src/EjemploArreglos.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class EjemploArreglos {
+    public static void main(String[] args) {
+
+        String[] productos = new String[7];
+        int total = productos.length;
+
+        productos[0] = "Kingston Pendrive 64GB";
+        productos[1] = "Samsung Galaxy";
+        productos[2] = "Disco Duro SSD Samsung Externo";
+        productos[3] = "Asus Notebook";
+        productos[4] = "Macbook Air";
+        productos[5] = "Chromecast 4ta generación";
+        productos[6] = "Bicicleta Oxford";
+
+        Arrays.sort(productos);
+
+        }
+
+
+}
